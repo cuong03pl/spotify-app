6e5a001aebd456f4744cb73c5b80d1020ac91b67
cuong03pl <cuongculebarca@gmail.com>
2023-03-17 15:31:11 +0700

    UI Dialog

---
+++ src/components/Dialog/Dialog.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import EditDetails from "components/EditDetails/EditDetails";
+
+export default function AlertDialog({ isOpen, onClick, id, data }) {
+  return (
+    <Dialog open={isOpen} onClose={onClick}>
+      <DialogContent sx={{ padding: 0, bgcolor: "#282828" }}>
+        <EditDetails id={id} onClick={onClick} data={data} />
+      </DialogContent>
+      {/* <DialogActions></DialogActions> */}
+    </Dialog>
+  );
+}
